454e1cf58258049227b08f15963885afad211cd5
Younes <my_saidji@esi.dz>
2023-12-09 08:55:55 +0000

    Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,17 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import useAuth from './hooks/useAuth';
 import Chat from './pages/Chat';
+import LandingPage from './pages/LandingPage';
+import Dawarat from './pages/Dawarat';
 function App() {
   const {user}=useAuth()
   return (
-    <div className="App ">
+    <div className="App bg-white">
+
       <BrowserRouter>
       <Routes>
-        <Route path="/" />
+      <Route path="/" element={<LandingPage/>} />
+      <Route path="/Dawarat" element={<Dawarat/>} />
         <Route path="/login" element={!user ? <Login/>: <Navigate to="/Chat"/>} />
         <Route path="/Signup" element={!user ? <Signup/>: <Navigate to="/Chat"/>}/>
         <Route path="/Chat" element={user ? <Chat/> : <Navigate to="/login"/>}/>
